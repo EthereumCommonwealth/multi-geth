b7891d9f2ab9ff3cca19a3902f7d0379fbede732
Ellaismer <ellaismer@protonmail.ch>
2018-03-27 03:04:53 -0400

    Add EllaismGenesisHash enforcement

--- params/config.go
+++ params/config.go
@@ -27,6 +27,7 @@ import (
 var (
 	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
 	TestnetGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
+	EllaismGenesisHash = common.HexToHash("0x4d7df65052bb21264d6ad2d6fe2d5578a36be12f71bf8d0559b0c15c4dc539b5")
 )
 
 var (
