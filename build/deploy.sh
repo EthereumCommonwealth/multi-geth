aedec85522f704e700bb33f39eb4f0d3dbe27b88
Akira Takizawa <akx20000@protonmail.com>
2018-04-07 22:43:36 +0900

    Config deploy & version

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-GETH_ARCHIVE_NAME="multi-geth-$TRAVIS_OS_NAME"
+GETH_ARCHIVE_NAME="multi-geth-$TRAVIS_OS_NAME-$TRAVIS_TAG"
 zip -j "$GETH_ARCHIVE_NAME.zip" build/bin/geth
-
-shasum -a 256 $GETH_ARCHIVE_NAME.zip
-shasum -a 256 $GETH_ARCHIVE_NAME.zip > $GETH_ARCHIVE_NAME.zip.sha256
