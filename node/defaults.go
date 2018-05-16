e237a4665fd183f0dfb53d3ed5c815a83a58beea
Akira Takizawa <akx20000@protonmail.com>
2018-05-17 03:12:31 +0900

    Move default datadir

--- node/defaults.go
+++ node/defaults.go
@@ -57,11 +57,11 @@ func DefaultDataDir() string {
 	home := homeDir()
 	if home != "" {
 		if runtime.GOOS == "darwin" {
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "EthereumSocial")
 		} else if runtime.GOOS == "windows" {
-			return filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			return filepath.Join(home, "AppData", "Roaming", "EthereumSocial")
 		} else {
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".ethereumsocial")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
