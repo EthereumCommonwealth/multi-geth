504b6ba97965180fd83d0919ca9f3a089369baa0
Akira Takizawa <akx20000@protonmail.com>
2018-05-17 03:05:48 +0900

    Use Ethereum Social as a default

--- core/genesis.go
+++ core/genesis.go
@@ -159,8 +159,8 @@ func SetupGenesisBlock(db ethdb.Database, genesis *Genesis) (*params.ChainConfig
 	stored := rawdb.ReadCanonicalHash(db, 0)
 	if (stored == common.Hash{}) {
 		if genesis == nil {
-			log.Info("Writing default main-net genesis block")
-			genesis = DefaultGenesisBlock()
+			log.Info("Writing default ethereumsocial main-net genesis block")
+			genesis = DefaultSocialGenesisBlock()
 		} else {
 			log.Info("Writing custom genesis block")
 		}
